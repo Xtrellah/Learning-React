2d176f7d2778955797f3c034a718b2c6f8142699
Max Estrella <maxestrella565@gmail.com>
2024-06-27 09:58:22 +0100

    Addition of button

--- src/components/DogPic/index.jsx
+++ src/components/DogPic/index.jsx
@@ -4,19 +4,24 @@ import { useEffect, useState } from "react"
 function DogPic() {
     const [pic, setPic] = useState('')
 
-    useEffect(() => {
+
+    useEffect(getDog, [])
+
+    function getDog() {
         fetch('https://dog.ceo/api/breeds/image/random')
             .then(res => res.json())
             .then(picData => {
-                setPic(picData.pic)
+                setPic(picData.message)
             })
-    }, [])
+    }
 
+    
     return (
-        <>
+        <div>
             <h2>Random Dog</h2>
-            <img src={pic} alt="huh" />
-        </>
+            <img src={pic} />
+            <button onClick={getDog}>Get new dog</button>
+        </div>
     )
 }
 
