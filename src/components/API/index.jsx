0aad902d52780702f215f6f236f992888f2f1ef4
Max Estrella <maxestrella565@gmail.com>
2024-06-27 11:58:24 +0100

    Api working better

--- src/components/API/index.jsx
+++ src/components/API/index.jsx
@@ -2,27 +2,34 @@ import { useEffect, useState } from "react"
 
 
 function API() {
+    const [image, setImage] = useState('')
     const [id, setId] = useState('')
     const [markethashname, setMarkethashname] = useState('')
     const [pricelatest, setPricelatest] = useState('')
 
 
-    useEffect(() => {
+    useEffect(getData, [])
+        
+    function getData() {
         fetch('https://www.steamwebapi.com/steam/api/inventory?key=D47J0TUU3MPQFZLK&steam_id=76561198414532849')
             .then(res => res.json())
             .then(Data => {
                 let game = Data['0']
                 console.log(game)
+                setImage(game.image)
                 setId(game.id)
                 setMarkethashname(game.markethashname)
                 setPricelatest(game.pricelatest)
             })
-    }, [])
+    }
 
     return (
         <>
             <h1>API</h1>
-            <p>ID: {id} Name: {markethashname} Price: {pricelatest}</p>
+            <img src={image}/>
+            <p>ID: {id}</p>
+            <p>Name: {markethashname}</p>
+            <p>Price: {pricelatest}</p>
         </>
     )
 }
